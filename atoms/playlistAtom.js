9e0431f6d940880db1ad23605acbd000bf7a9293
Jay Sierra <jsierra99@live.com>
2021-12-15 20:45:14 -0800

    spotify

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const playlistState = atom({
+    key: "playlistState",
+    default: null,
+});
+
+export const playlistIdState = atom({
+    key: "playlistIdState",
+    default: "6HKDqcV4G0IwUSh7kKL93Q"
+});//2:30
